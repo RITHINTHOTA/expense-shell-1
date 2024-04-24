74c7ab53ef0b671487092aa8446768409c662dec
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-24 13:22:41 +0530

    commit

--- frontend.sh
+++ frontend.sh
@@ -22,7 +22,7 @@ cd /usr/share/nginx/html &>>$LOGFILE
 unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE "$?" "Extracting file"
 
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/expense-shell-1/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE "$?" "Copied expense.conf"
 
 systemctl restart nginx &>>$LOGFILE
