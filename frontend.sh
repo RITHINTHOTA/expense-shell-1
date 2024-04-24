1dfb77e492e771af1360f6c06eb9d170caddbb8d
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-24 13:20:15 +0530

    commit

--- frontend.sh
+++ frontend.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
+source ./common.sh
 
-TIMESTAMP=$( date +%F-%H-%M-%S )
-SCRIPTNAME=$( echo $0 | cut -d "." -f1 )
-LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-echo "please enter DB password"
-read mysql_root_expense
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-       echo -e "$2....$R FAILURE $N"
-    else
-       echo -e "$2....$G Success $N"
-    fi
-}
-USERID=$( id -u )
-if [ $USERID -ne 0 ]
-then
-    echo "please run with root access"
-    exit 1
-else
-    echo "your a root user"
-fi
+check_root
 
 dnf install nginx -y  &>>$LOGFILE
 VALIDATE "$?" "Installing nginx"
