9868053a060c3af102baa386b3020b9ce5b54005
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-24 13:19:46 +0530

    commit

--- backend.sh
+++ backend.sh
@@ -39,7 +39,7 @@ VALIDATE "$?" "Extracted backend code"
 npm install &>>$LOGFILE
 VALIDATE "$?" "Installing nodejs dependencies"
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE "$?" "Copied backend.service"
 
 systemctl daemon-reload &>>$LOGFILE
